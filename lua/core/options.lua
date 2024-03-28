a9084b32287e766d6b80fef73d4f4bfb4522f426
Sergiu Nani <130356254+SergiuNani@users.noreply.github.com>
2024-03-28 23:53:20 +0200

    0326202406

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,13 +3,10 @@ vim.cmd("autocmd BufNewFile,BufRead *.tml,*.cfg,*.cp set ft=c")
 
 vim.g.have_nerd_font = true
 vim.opt.showmode = false
-vim.opt.signcolumn = "yes"
-vim.opt.inccommand = "split"
 
-vim.opt.scrolloff = 4 -- is one of my fav
  local opt = vim.opt -- for conciseness
-
 opt.fillchars = { fold = " " }
+
 -- BSSSSSSSSSSSSSSSSSSSSS =======================
 opt.foldmethod = "indent"
 opt.foldcolumn = '0'
@@ -19,54 +16,103 @@ opt.foldenable = true
 opt.foldopen = block
 -- BSSSSSSSSSSSSSSSSSSSSS =======================
 
-opt.undofile = false -- Once you save the file  and close you cannot go further in history , enabling it does the opp
--- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
 
--- line wrapping
-opt.wrap = false -- disable line wrapping
 
--- line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
-opt.relativenumber = true -- show relative line numbers
 
--- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
-
--- line wrapping
-opt.wrap = false -- disable line wrapping
-
--- search settings
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = false -- if you include mixed case in your search, assumes you want case-sensitive
-
--- cursor line
-opt.cursorline = true -- highlight the current cursor line
 
 -- appearance
 
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
-opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
--- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+local options = {
+    ai = true,
+    autoindent = true,
+    autowrite = true,
+    backspace = 'indent,eol,start',
+    backup = false, -- creates a backup file
+    breakindent = true,
+    clipboard = 'unnamedplus', -- allows neovim to access the system clipboard
+    cmdheight = 1, -- more space in the neovim command line for displaying messages
+    completeopt = 'menu,menuone,noselect', -- mostly just for cmp
+    -- conceallevel = 0, -- so that `` is visible in markdown files
+    confirm = true, -- Confirm to save changes before exiting modified buffer
+    cursorline = true, -- highlight the current line
+    expandtab = true, -- convert tabs to spaces
+    fileencoding = 'utf-8', -- the encoding written to a file
+    formatoptions = 'jlnqt', -- set formatoptions, check help fo-table
+    grepformat = '%f:%l:%c:%m',
+    grepprg = 'rg --vimgrep',
+    hlsearch = true, -- highlight all matches on previous search pattern
+    ignorecase = true, -- ignore case in search patterns
+    inccommand = 'split', -- preview incremental substitute
+    laststatus = 3,
+    list = true,
+    listchars = { trail = '', tab = '', nbsp = '_', extends = '>', precedes = '<' }, -- highlight
+    mouse = 'a', -- allow the mouse to be used in neovim
+    number = true, -- set numbered lines
+  relativenumber = true, -- set relative numbered lines
+    numberwidth = 4, -- set number column width to 2 {default 4}
+    pumblend = 10, -- Popup blen
+    pumheight = 10, -- pop up menu height
+    scrolloff = 4, -- is one of my fav
+    sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal',
+    shiftround = true, -- Round indent
+    shiftwidth = 4, -- the number of spaces inserted for each indentation --maybe set it to 2
+    showcmd = false,
+    showmode = false, -- we don't need to see things like -- INSERT -- anymore
+    showtabline = 0, -- always show tabs
+    si = true,
+    sidescrolloff = 8,
+    signcolumn = 'yes', -- always show the sign column, otherwise it would shift the text each time
+    smartcase = false, -- smart case
+    smartindent = true, -- make indenting smarter again
+    smarttab = true,
+    splitbelow = true, -- force all horizontal splits to go below current window
+    splitright = true, -- force all vertical splits to go to the right of current window
+    swapfile = false, -- creates a swapfile
+    tabstop = 4, -- insert 2 spaces for a tab
+    termguicolors = true, -- set term gui colors (most terminals support this)
+    timeoutlen = 300, -- time to wait for a mapped sequence to complete (in milliseconds)
+    undofile = false, -- enable persistent undo
+    undolevels = 10000,
+    updatetime = 50, -- faster completion (4000ms default)
+    wildmenu = true, -- wildmenu
+    wildmode = 'longest:full,full', -- Command-line completion mode
+    winminwidth = 5, -- Minimum window width
+    wrap = false, -- display lines as one long line
+    writebackup = false, -- do not edit backups
+}
 
--- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+vim.cmd([[
+     setlocal spell spelllang=en "Set spellcheck language to en
+     setlocal spell! "Disable spell checks by default
+     filetype plugin indent on
+     if has('win32')
+        let g:python3_host_prog = $HOME . '/scoop/apps/python/current/python.exe'
+     endif
+ ]])
 
--- turn off swapfile
-opt.swapfile = false
+vim.opt.path:append({ '**' })
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+vim.opt.shortmess:append({ W = true, I = true, c = true })
+
+if vim.fn.has('nvim-0.9.0') == 1 then
+    vim.opt.splitkeep = 'screen'
+    vim.opt.shortmess:append({ C = true })
+end
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+vim.cmd('set whichwrap+=<,>,[,],h,l')
+vim.cmd([[set iskeyword+=-]])
 
--- lua/plugins/list.lua
+++ lua/plugins/list.lua
@@ -1,319 +1,314 @@
 local function load_config(package)
-  return function()
-    require('plugins.' .. package)
-  end
+    return function()
+        require('plugins.' .. package)
+    end
 end
 
 local plugins = {
-  ---------------- UI ----------------
-  {
-    config = load_config('ui.onedark'),
-    'navarasu/onedark.nvim',
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    config = load_config('tools.harpoon'),
-    event = { 'BufReadPre', 'BufNewFile', 'VimEnter' },
-    priority=1000,
-  },
-
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-      "kevinhwang91/promise-async",
+    ---- ======================== First Priority  =======================
+    {
+        config = load_config('ui.onedark'),
+        'navarasu/onedark.nvim',
+        lazy = false,
+        priority = 1000,
     },
-    config = load_config('tools.ufo'),
-    event = { 'VimEnter', 'BufNewFile' },
-  },
-  {
-    'echasnovski/mini.comment',
-    version = '*',
-    dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
-    config = load_config('lang.comment'),
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
-  -- {
-  "bluz71/vim-nightfly-guicolors",--ehh
-  "folke/tokyonight.nvim",--ehh
-  'olimorris/onedarkpro.nvim',
-  "rebelot/kanagawa.nvim", --5/10
-  "ribru17/bamboo.nvim", --6/10
-  "rose-pine/neovim", --3/10
-  "ellisonleao/gruvbox.nvim", --7/10
-  "marko-cerovac/material.nvim", --2/10
-  config = load_config('ui.colorscheme'),
-  lazy = false,
-  priority = 1000,
-  -- },
-  {
-    'nvim-lualine/lualine.nvim',
-    config = load_config('ui.lualine'),
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    config = load_config('ui.indentline'),
-    main = 'ibl',
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
-  {
-    "shellRaining/hlchunk.nvim",
-    event = { "UIEnter" },
-    config = load_config('ui.hlchunk'),
-  },
-  {
-    'HiPhish/rainbow-delimiters.nvim',
-    config = load_config('ui.rainbow'),
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
-  {
-    'rcarriga/nvim-notify',
-    config = load_config('ui.notify'),
-    event = 'VeryLazy',
-    cmd = 'Notifications',
-  },
-  {
-    'stevearc/dressing.nvim',
-    --Basically wraps UI around renaming/adding files in nvim-tree
-    config = load_config('ui.dressing'),
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
-  {
-    'folke/zen-mode.nvim',
-    dependencies = {
-      'folke/twilight.nvim',
-      config = load_config('ui.twilight'),
+    -- Telescope
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build = 'make',
+            },
+            'molecule-man/telescope-menufacture',
+            'debugloop/telescope-undo.nvim',
+        },
+        config = load_config('tools.telescope'),
+        cmd = 'Telescope',
     },
-    config = load_config('ui.zen-mode'),
-    cmd = { 'ZenMode' ,'Twilight' },
-  },
 
-  -- --  ---------------- Language ----------------
-  {
-    'windwp/nvim-autopairs',
-    config = load_config('lang.autopairs'),
-    event = 'InsertEnter',
-  },
-
-  -- -- {
-  -- --     'echasnovski/mini.surround',
-  -- --     version = '*',
-  -- --     config = load_config('lang.surround'),
-  -- --     event = { 'BufReadPre', 'BufNewFile' },
-  -- -- },
-  -- -- {
-  -- --     'echasnovski/mini.ai',
-  -- --     version = '*',
-  -- --     config = load_config('lang.ai'),
-  -- --     event = { 'BufReadPre', 'BufNewFile' },
-  -- -- },
-
-  -- -- ---------------- Tresitter ----------------
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-refactor',
-      'nvim-treesitter/nvim-treesitter-textobjects',
-      'RRethy/nvim-treesitter-endwise',
-      'RRethy/nvim-treesitter-textsubjects',
-      'windwp/nvim-ts-autotag',
+    --  Tresitter ----------------
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-refactor',
+            'nvim-treesitter/nvim-treesitter-textobjects',
+            'RRethy/nvim-treesitter-endwise',
+            'RRethy/nvim-treesitter-textsubjects',
+            'windwp/nvim-ts-autotag',
+        },
+        config = load_config('lang.treesitter'),
+        event = { 'BufReadPre', 'BufNewFile' },
     },
-    config = load_config('lang.treesitter'),
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
-  {
-    'ckolkey/ts-node-action',
-    dependencies = { 'nvim-treesitter' },
-  },
-  -- --  ----------------LSP ----------------
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      { "antosha417/nvim-lsp-file-operations", config = true },
-      { "j-hui/fidget.nvim", opts = {} },
-      { 'hrsh7th/cmp-nvim-lsp',opts={}},
-    },
-    config = load_config('lang.lspconfig'),
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
-  {
-    'folke/neodev.nvim',
-    ft = { 'lua', 'vim' },
-    config = load_config('lang.neodev'),
-  },
-  {
-    'nvimdev/lspsaga.nvim',
-    config = load_config('lang.lspsaga'),
-    event = 'LspAttach',
-  },
-  {
-    'Maan2003/lsp_lines.nvim',
-    config = load_config('lang.lsp-lines'),
-    event = 'LspAttach',
-  },
-  {
-    'williamboman/mason.nvim',
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = load_config('lang.mason'),
-    cmd = 'Mason',
-  },
 
-  -- -- ---------------- Completion ----------------
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-nvim-lua',
-      'L3MON4D3/LuaSnip',
-      'rafamadriz/friendly-snippets',
-      'saadparwaiz1/cmp_luasnip',
+    {
+        'nvim-tree/nvim-tree.lua',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = load_config('tools.nvim-tree'),
+        cmd = 'NvimTreeToggle',
     },
-    config = load_config('lang.cmp'),
-    event = 'InsertEnter',
-  },
-  -- {
-  --     'zbirenbaum/copilot.lua',
-  --     dependencies = {
-  --         'zbirenbaum/copilot-cmp',
-  --     },
-  --     config = load_config('lang.copilot'),
-  --     event = 'InsertEnter',
-  -- },
-
-  -- --  ---------------- Tools ----------------
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
+    --  Completion ----------------
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/cmp-nvim-lua',
+            'L3MON4D3/LuaSnip',
+            'rafamadriz/friendly-snippets',
+            'saadparwaiz1/cmp_luasnip',
+        },
+        config = load_config('lang.cmp'),
+        event = 'InsertEnter',
     },
-    config = load_config('tools.nvim-tree'),
-    cmd = 'NvimTreeToggle',
-  },
-  {
-    'windwp/nvim-spectre',
-    config = load_config('tools.spectre'),
-    cmd = 'Spectre',
-  },
-  -- -- { --???? to investigate
-  -- --     "mg979/vim-visual-multi",
-  -- --     config = load_config('tools.VimVisualMulti'),
-
-  -- -- },
-  {
-    "NvChad/nvim-colorizer.lua",
-    event = { "BufReadPre", "BufNewFile" },
-    config = true,
-  },
-  {
-    'folke/flash.nvim',
-    config = load_config('tools.flash'),
-    keys = {
-      {
-        's',
-        mode = { 'n', 'x', 'o' },
-        function()
-          require('flash').jump()
-        end,
-        desc = 'Flash',
-      },
-      {
-        'S',
-        mode = { 'n', 'x', 'o' },
-        function()
-          require('flash').treesitter()
-        end,
-        desc = 'Flash Treesitter',
-      },
-      {
-        'r',
-        mode = 'o',
-        function()
-          require('flash').remote()
-        end,
-        desc = 'Remote Flash',
-      },
-      {
-        'R',
-        mode = { 'o', 'x' },
-        function()
-          require('flash').treesitter_search()
-        end,
-        desc = 'Treesitter Search',
-      },
-      {
-        '<c-s>',
-        mode = { 'c' },
-        function()
-          require('flash').toggle()
-        end,
-        desc = 'Toggle Flash Search',
-      },
+    {
+        'ThePrimeagen/harpoon',
+        branch = 'harpoon2',
+        config = load_config('tools.harpoon'),
+        event = { 'BufReadPre', 'BufNewFile', 'VimEnter' },
+        priority=1000,
     },
-  },
-  {
-    'm4xshen/hardtime.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('hardtime').setup({ enabled = true })
-    end,
-    cmd = 'Hardtime',
-  },
-  {
-    'folke/which-key.nvim',
-    config = load_config('tools.which-key'),
-    event = 'VeryLazy',
-  },
-  {
-    '2kabhishek/termim.nvim',
-    cmd = { 'Fterm', 'FTerm', 'Sterm', 'STerm', 'Vterm', 'VTerm' },
-  },
-
-  -- Telescope
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-      },
-      'nvim-telescope/telescope-symbols.nvim',
-      'molecule-man/telescope-menufacture',
-      'debugloop/telescope-undo.nvim',
+    {
+        'echasnovski/mini.comment',
+        version = '*',
+        dependencies = 'JoosepAlviste/nvim-ts-context-commentstring',
+        config = load_config('lang.comment'),
+        event = { 'BufReadPre', 'BufNewFile' },
     },
-    config = load_config('tools.telescope'),
-    cmd = 'Telescope',
-  },
 
-  -- -- -- Git
-  {
-    'lewis6991/gitsigns.nvim',
-    cmd = 'Gitsigns',
-    config = load_config('tools.gitsigns'),
-    event = { 'BufReadPre', 'BufNewFile' },
-  },
+    {
+        'windwp/nvim-autopairs',
+        config = load_config('lang.autopairs'),
+        event = 'InsertEnter',
+    },
+    {
+        "kevinhwang91/nvim-ufo",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+        config = load_config('tools.ufo'),
+        event = { 'VimEnter', 'BufNewFile' },
+    },
 
+    {
+        'folke/which-key.nvim',
+        config = load_config('tools.which-key'),
+        event = 'VeryLazy',
+    },
+    {
+        'folke/flash.nvim',
+        config = load_config('tools.flash'),
+        keys = {
+            {
+                's',
+                mode = { 'n', 'x', 'o' },
+                function()
+                    require('flash').jump()
+                end,
+                desc = 'Flash',
+            },
+            {
+                'S',
+                mode = { 'n', 'x', 'o' },
+                function()
+                    require('flash').treesitter()
+                end,
+                desc = 'Flash Treesitter',
+            },
+            {
+                'r',
+                mode = 'o',
+                function()
+                    require('flash').remote()
+                end,
+                desc = 'Remote Flash',
+            },
+            {
+                'R',
+                mode = { 'o', 'x' },
+                function()
+                    require('flash').treesitter_search()
+                end,
+                desc = 'Treesitter Search',
+            },
+            {
+                '<c-s>',
+                mode = { 'c' },
+                function()
+                    require('flash').toggle()
+                end,
+                desc = 'Toggle Flash Search',
+            },
+        },
+    },
+
+    -- ======================== Secondary Priority  =======================
+
+    {
+        '2kabhishek/termim.nvim', -- For lazyGit
+        cmd = { 'Fterm', 'FTerm', 'Sterm', 'STerm', 'Vterm', 'VTerm' },
+    },
+    {
+        'lewis6991/gitsigns.nvim',
+        cmd = 'Gitsigns',
+        config = load_config('tools.gitsigns'),
+        event = { 'BufReadPre', 'BufNewFile' },
+    },
+    -- --  ----------------LSP ----------------
+    {
+        'williamboman/mason.nvim',
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
+        config = load_config('lang.mason'),
+        cmd = 'Mason',
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            { "antosha417/nvim-lsp-file-operations", config = true },
+            { "j-hui/fidget.nvim", opts = {} }, -- Right bottom corner when enter it shows the %  of LSP
+            { 'hrsh7th/cmp-nvim-lsp',opts={}},
+        },
+        config = load_config('lang.lspconfig'),
+        event = { 'BufReadPre', 'BufNewFile' },
+    },
+    {
+        'nvimdev/lspsaga.nvim',
+        config = load_config('lang.lspsaga'),
+        event = 'LspAttach',
+    },
+
+    -- ======================== Third Priority =======================
+    {
+        'folke/zen-mode.nvim',
+        dependencies = {
+            'folke/twilight.nvim',
+            config = load_config('ui.twilight'),
+        },
+        config = load_config('ui.zen-mode'),
+        cmd = { 'ZenMode' ,'Twilight' },
+    },
+
+    {
+        'windwp/nvim-spectre',
+        config = load_config('tools.spectre'),
+        cmd = 'Spectre',
+    },
+    {
+        "NvChad/nvim-colorizer.lua",
+        event = { "BufReadPre", "BufNewFile" },
+        config = true,
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        config = load_config('ui.lualine'),
+        event = { 'BufReadPre', 'BufNewFile' },
+    },
+    {
+        'lukas-reineke/indent-blankline.nvim',
+        config = load_config('ui.indentline'),
+        main = 'ibl',
+        event = { 'BufReadPre', 'BufNewFile' },
+    },
+    {
+        "shellRaining/hlchunk.nvim",
+        event = { "UIEnter" },
+        config = load_config('ui.hlchunk'),
+    },
+    {
+        'HiPhish/rainbow-delimiters.nvim',
+        config = load_config('ui.rainbow'),
+        event = { 'BufReadPre', 'BufNewFile' },
+    },
+    {
+        'rcarriga/nvim-notify',
+        config = load_config('ui.notify'),
+        event = 'VeryLazy',
+        cmd = 'Notifications',
+    },
+    {
+        'stevearc/dressing.nvim',
+        --Basically wraps UI around renaming/adding files in nvim-tree
+        config = load_config('ui.dressing'),
+        event = { 'BufReadPre', 'BufNewFile' },
+    },
+
+    -- -- { --???? to investigate
+    -- --     "mg979/vim-visual-multi",
+    -- --     config = load_config('tools.VimVisualMulti'),
+
+    -- -- },
+    -- {
+    --     'Maan2003/lsp_lines.nvim', -- IDK about this one
+    --     config = load_config('lang.lsp-lines'),
+    --     event = 'LspAttach',
+    -- },
+
+
+        -- -- {
+    -- --     'echasnovski/mini.surround',
+    -- --     version = '*',
+    -- --     config = load_config('lang.surround'),
+    -- --     event = { 'BufReadPre', 'BufNewFile' },
+    -- -- },
+    -- -- {
+    -- --     'echasnovski/mini.ai',
+    -- --     version = '*',
+    -- --     config = load_config('lang.ai'),
+    -- --     event = { 'BufReadPre', 'BufNewFile' },
+    -- -- },
+
+    -- {
+    --     'm4xshen/hardtime.nvim',
+    --     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    --     config = function()
+    --         require('hardtime').setup({ enabled = true })
+    --     end,
+    --     cmd = 'Hardtime',
+    -- },
+
+    -- {
+    -- "bluz71/vim-nightfly-guicolors",--ehh
+    -- "folke/tokyonight.nvim",--ehh
+    -- 'olimorris/onedarkpro.nvim',
+    -- "rebelot/kanagawa.nvim", --5/10
+    -- "ribru17/bamboo.nvim", --6/10
+    -- "rose-pine/neovim", --3/10
+    -- "ellisonleao/gruvbox.nvim", --7/10
+    -- "marko-cerovac/material.nvim", --2/10
+    -- config = load_config('ui.colorscheme'),
+    -- lazy = false,
+    -- priority = 1000,
+    -- },
+
+
+    -- {
+    --     'zbirenbaum/copilot.lua',
+    --     dependencies = {
+    --         'zbirenbaum/copilot-cmp',
+    --     },
+    --     config = load_config('lang.copilot'),
+    --     event = 'InsertEnter',
+    -- },
 
 }
 
 local lsp_servers = {
-  -- 'eslint',
-  -- 'emmet_ls',
-  'html',
-  'cssls',
-  'jsonls',
-  'lua_ls',
-  'tsserver',
+    -- 'eslint',
+    -- 'emmet_ls',
+    'html',
+    'cssls',
+    'jsonls',
+    'lua_ls',
+    'tsserver',
 }
 
 -- local null_ls_sources = {
@@ -321,23 +316,23 @@ local lsp_servers = {
 -- }
 
 local ts_parsers = {
-  'css',
-  'gitcommit',
-  "javascript",
-  "tsx",
-  'html',
-  "xml",
-  'javascript',
-  'json',
-  'lua',
-  "c",
-  "c_sharp",
-  'typescript',
+    'css',
+    'gitcommit',
+    "javascript",
+    "tsx",
+    'html',
+    "xml",
+    'javascript',
+    'json',
+    'lua',
+    "c",
+    "c_sharp",
+    'typescript',
 }
 
 return {
-  plugins = plugins,
-  lsp_servers = lsp_servers,
-  null_ls_sources = null_ls_sources,
-  ts_parsers = ts_parsers,
+    plugins = plugins,
+    lsp_servers = lsp_servers,
+    null_ls_sources = null_ls_sources,
+    ts_parsers = ts_parsers,
 }
