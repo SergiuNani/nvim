4c993dd9004a47d9a5c0e2c67165a9ff54f7630b
Sergiu N <nsergiu97@gmail.com>
2024-04-27 22:47:25 +0300

    0427202402

---
+++ lua/plugins/ui/bookmarks.lua
@@ -0,0 +1,10 @@
+ -- "LintaoAmons/bookmarks.nvim",
+
+require("bookmarks").setup({})
+      -- json_db_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/bookmarks.db.json"),
+vim.keymap.set({ "n", "v" }, "mm", "<cmd>BookmarksMark<cr>", { desc = "Mark current line into active BookmarkList." })
+vim.keymap.set({ "n", "v" }, "ml", "<cmd>BookmarksGoto<cr>", { desc = "Go to bookmark at current active BookmarkList" })
+vim.keymap.set({ "n", "v" }, "mf", "<cmd>BookmarksCommands<cr>", { desc = "Find and trigger a bookmark command." })
+vim.keymap.set({ "n", "v" }, "mg", "<cmd>BookmarksGotoRecent<cr>", { desc = "Go to latest visited/created Bookmark" })
+vim.keymap.set({ "n", "v" }, "ma", "<cmd>BookmarksSetActiveList<cr>", { desc = "Go to latest visited/created Bookmark" })
+
